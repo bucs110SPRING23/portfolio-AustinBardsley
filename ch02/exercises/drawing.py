e2435c140ab6d611acf2f69deec25f88047ba4fd
Austin Bardsley <abardsl1@binghamton.edu>
2023-02-09 16:34:53 -0500

    drawing.2

--- ch02/exercises/drawing.py
+++ ch02/exercises/drawing.py
@@ -5,7 +5,8 @@ length = float(input("Enter Side Length: "))
 my_turtle = turtle.Turtle()
 my_turtle.color("red")
 my_turtle.shape("turtle")
-for i in range (sides):
+my_turtle.speed(0)
+for _ in range (sides):
     my_turtle.fd(length)
     my_turtle.left(180-((sides-2)*180/sides))
 turtle.exitonclick()
