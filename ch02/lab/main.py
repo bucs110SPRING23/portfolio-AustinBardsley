d4dfad6eed41b20e2ef1b90a3052517d8ffcb032
Austin Bardsley <abardsl1@binghamton.edu>
2023-02-20 11:04:45 -0500

    lab2

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,10 +1,75 @@
+import turtle
+import random
+import pygame
+import math
 
 #Part A
-weeks = 16
-classes = 5
-tuition = 6000
-cost_per_week = ((tuition / classes) / weeks)
-print("Cost per week:", cost_per_week)
+window = turtle.Screen() # 2.  Create a screen
+window.bgcolor('lightblue')
+
+michelangelo = turtle.Turtle() # 3.  Create two turtles
+leonardo = turtle.Turtle()
+michelangelo.color('orange')
+leonardo.color('blue')
+michelangelo.shape('turtle')
+leonardo.shape('turtle')
+
+michelangelo.up() # 4. Pick up the pen so we don’t get lines
+leonardo.up()
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
+
+## 5. Your PART A code goes here
+# Race 1
+
+x = random.randrange(1,100)
+michelangelo.forward(x)
+y=random.randrange(1,100)
+leonardo.forward(y)
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
+
+# Race 2
+for _ in range(70):
+    x = random.randrange(1,10)
+    michelangelo.forward(x)
+    y=random.randrange(1,10)
+    leonardo.forward(y)
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
+window.exitonclick()
+
+# PART B - complete part B here
+
+import pygame
+import math
+
+pygame.init()
+while 1:
+    pygame.event.get()
+    window = pygame.display.set_mode()
+    window.fill("yellow")
+    num_sides = [3,4,6,20,100,360]
+    side_length = 200
+    xpos = 720
+    ypos = 500
+    for m in (num_sides):
+        points = []
+        for i in range(m):
+            angle = 360/m
+            radians = math.radians(angle*i)
+            x = xpos+side_length*math.cos(radians) 
+            y = ypos+side_length*math.sin(radians)
+            points.append([x,y])
+        
+        pygame.draw.polygon(window, "teal", points)
+        pygame.display.flip()
+        pygame.time.wait(1000)
+        window.fill("yellow")
+        pygame.display.flip()
+        points = []
+    break
 
 
-#Part B
+
+
