d4378aa3e626ca1b7b8d5dd55cdf3200906f4b1f
Austin Bardsley <abardsl1@binghamton.edu>
2023-03-06 17:07:42 -0500

    4/6/23(1)

--- ch04/exercises/coinflip.py
+++ ch04/exercises/coinflip.py
@@ -3,8 +3,11 @@ import turtle
 
 my_turtle = turtle.Turtle()
 turtle.setup(width=800, height=800, startx=0, starty=0)
+my_turtle.speed(0)
+y_range = turtle.window_height()/2
+x_range = turtle.window_width()/2
 
-while -400 <=my_turtle.xcor() <= 400 and -400<= my_turtle.ycor() <= 400:
+while abs(my_turtle.xcor()) < x_range and abs(my_turtle.ycor()) < y_range:
     heads_tails = int(random.randrange(2))
     if heads_tails == 0:
         my_turtle.left(90)
@@ -12,6 +15,5 @@ while -400 <=my_turtle.xcor() <= 400 and -400<= my_turtle.ycor() <= 400:
     else:
         my_turtle.right(90)
         my_turtle.forward(50)
-    print(my_turtle.position())
 
  
